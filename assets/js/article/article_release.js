1f6236a72ec703cd3875c1b33664b9f7956afa96
xiaojing520-ai <542258713@qq.com>
2020-10-11 17:46:18 +0800

    发布文章页面中的文章分类数据渲染完成

--- assets/js/article/article_release.js
+++ assets/js/article/article_release.js
@@ -16,4 +16,22 @@ $(function () {
 
   // 2.3 创建裁剪区域
   $image.cropper(options)
+
+  // 3. 发送ajax请求获取分类数据
+  // 3.1 立即发送ajax请示
+  $.ajax({
+    type: "GET",
+    url: "/my/article/cates",
+    success: function (res) {
+      console.log(res)
+      // 3.2 将数据渲染到下拉列表当中
+      if (res.status == 0) {
+        var htmlStr = template("categoryList", res)
+        $("#category").html(htmlStr)
+
+        // 重新渲染一下表单内容
+        layui.form.render()
+      }
+    }
+  })
 })
